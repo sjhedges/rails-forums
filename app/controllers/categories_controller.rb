dd4d22190389035f40a9378767bccb4dcd1d6108
Michael LaBahn <mdlabahn@yahoo.com>
2017-06-10 11:28:01 -0600

    category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,45 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, except: [:index, :new, :create]
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to category_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
