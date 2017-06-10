a533e6facd72795a45eb96491e50d06ce70e7e4e
Stockton <stocktonhedges@gmail.com>
2017-06-10 11:42:00 -0600

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'categories/new'
-
-  get 'categories/edit'
-
   root 'categories#index'
-
  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
