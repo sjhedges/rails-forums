2fa1ad42d13095666502a80a08a93ae49d7a82c2
Stockton <stocktonhedges@gmail.com>
2017-06-10 12:12:59 -0600

    models done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,62 @@
 class PostsController < ApplicationController
+  #before_action :set_post, except: [:index, :new, :create]
+
   def index
+    @category = Category.find(params[:category_id])
+    @posts = @category.posts
+
   end
 
   def show
-  end
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.find(params[:id])
 
-  def edit
   end
 
   def new
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.new(post_params)
+    if @post.save
+      redirect_to category_post_path(@category, @post), notice: 'Post Created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to category_post_path(@category, @post), notice: 'Post Successfully Edited!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:category_id])
+    post = @category.posts.find(params[:id])
+    post.destroy
+    redirect_to category_posts_path, notice: 'Post Successfully Deleted.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
+  def set_post
+    @post = @category.posts(params[:id])
   end
 end
