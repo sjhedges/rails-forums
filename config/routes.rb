307fd9b5658a6b6414ea3b6aed8c686b87a5166c
Ransom Valley <ransomwy@gmail.com>
2017-06-10 11:04:09 -0600

    merge routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
 Rails.application.routes.draw do
-  get 'comments/index'
+  devise_for :users
 
-  get 'comments/show'
+  resources :categories do
+    resources :posts
+  end
 
-  get 'comments/edit'
-
-  get 'comments/new'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/new'
-
-  get 'catagories/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170610161723) do
+=======
+ActiveRecord::Schema.define(version: 20170610160807) do
+>>>>>>> devise progress
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+<<<<<<< HEAD
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -38,13 +43,32 @@ ActiveRecord::Schema.define(version: 20170610161723) do
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
+=======
+>>>>>>> devise progress
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+<<<<<<< HEAD
   end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "posts", "categories"
+=======
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+>>>>>>> devise progress
 end
