40e459a7d228df9d1aea49cac4eb966ecdb02e61
Stockton <stocktonhedges@gmail.com>
2017-06-10 17:03:31 -0600

    dont work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,8 +32,6 @@ class CommentsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     if @comment.update(comment_params)
       redirect_to post_comment_path(@post, @comment), notice: 'Comment Successfully Edited!'
     else
@@ -56,6 +54,6 @@ class CommentsController < ApplicationController
 
   def set_comment
     @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
   end
 end
